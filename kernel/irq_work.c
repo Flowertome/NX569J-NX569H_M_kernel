Bauuuuu/android_kernel_nubia_nx512j

arm64: use the new *_relaxed macros for lower power usage <franciscofranco.1990@gmail.com>

kernel/irq_work.c
@@ -91,7 +91,7 @@ bool irq_work_needs_cpu(void)
 	struct llist_head *this_list;
 
 	this_list = &__get_cpu_var(irq_work_list);
-	if (llist_empty(this_list))
+	if (llist_empty_relaxed(this_list))
 		return false;
 
 	/* All work should have been flushed before going offline */
@@ -116,7 +116,7 @@ static void __irq_work_run(void)
 	barrier();
 
 	this_list = &__get_cpu_var(irq_work_list);
-	if (llist_empty(this_list))
+	if (llist_empty_relaxed(this_list))
 		return;
 
 	BUG_ON(!irqs_disabled());

Flowertome/NX569J-NX569H_M_kernel

reviewed-off-by: Flowertome <gesangtome@foxmail.com>
Signed-off-by: Flowertome <gesangtome@foxmail.com>

kernel/irq_work.c
@@ -113,7 +113,7 @@ bool irq_work_needs_cpu(void)
 
 	raised = &__get_cpu_var(raised_list);
 	lazy = &__get_cpu_var(lazy_list);
-	if (llist_empty(raised) && llist_empty(lazy))
+	if (llist_empty_relaxed(raised) && llist_empty_relaxed(lazy))
 		return false;
 
 	/* All work should have been flushed before going offline */
@@ -130,7 +130,7 @@ static void irq_work_run_list(struct llist_head *list)
 
 	BUG_ON(!irqs_disabled());
 
-	if (llist_empty(list))
+	if (llist_empty_relaxed(list))
 		return;
 
 	llnode = llist_del_all(list);